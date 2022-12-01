f5730f203ec0540ef3e39abc22789a824958c756
princi <princijain99@gmail.com>
2022-12-01 18:37:13 +0530

    this is commit

---
+++ function1.js
@@ -0,0 +1,38 @@
+// 1st function
+var a = 35;
+var b = 25;
+function add() {
+    sum = a + b;
+    console.log(sum);
+}
+add();
+
+
+// 2nd function
+function minus() {
+    sub = a - b;
+    console.log(sub)
+}
+minus()
+
+
+// 3rd function
+function multiply() {
+    mul = b * a;
+    console.log(mul)
+}
+multiply()
+
+// 4th function
+function division() {
+    divide = b / a;
+    console.log(divide)
+}
+division()
+
+// 5th function
+function add(a, b) {
+    sum = a + b;
+    console.log(sum)
+}
+add(25, 5)
